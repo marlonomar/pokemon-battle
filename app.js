144e6a448ac5f0b98c323a326b292ee17f32bf76
Garcia <marlon.omar.g.garcia@accenture.com>
2019-05-22 18:42:22 -0300

    geroku

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/src'))
 
-app.listen(3000);
-console.log("abrir site en el puerto 3000")
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log("inicio del programa...")
+    console.log("abrir localhost:3000...")
+});
