c8a283eec98160c337ca29b42637c3397d2897df
Garcia <marlon.omar.g.garcia@accenture.com>
2019-05-22 18:38:51 -0300

    listo para subir a geroku

--- output/app.js
+++ output/app.js
@@ -1,1 +1,9 @@
-"use strict";
+const http = require('http');
+
+http.createServer((req,res)=>{
+    res.write("hola mundo");
+    res.end();
+})
+.listen(3000)
+
+console.log("escuchando en el puerto 3000")
